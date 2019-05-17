3c8a39ac163e098ea6d3ffe2b6187260ea6faf36
marsuraa <tony2parker0@gmail.com>
2019-05-17 17:03:36 +0200

    Update connexionController.php

--- controller/connexionController.php
+++ controller/connexionController.php
@@ -20,7 +20,6 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && $_SESSION['u
 
 		}
 		
-		$user = array('per_id' => $result[0][0] , 'per_user' =>$result[0][1],'per_password' =>$result[0][2],'per_nom' =>$result[0][3],'per_mail' =>$result[0][4],'etu_id' => $res[0][0], 'type' =>'etudiant');
 
 		if($i==1)
 		{
@@ -29,12 +28,12 @@ if (isset($_SESSION['username']) && isset($_SESSION['password']) && $_SESSION['u
 
 		if($i==2)
 		{
-			$user = array('per_id' => $result[0][0] , 'per_user' =>$result[0][1],'per_password' =>$result[0][2],'per_nom' =>$result[0][3],'per_mail' =>$result[0][4],'etu_id' => $res[0][0], 'type' =>'etudiant','pro_id' =>$res[0][0], 'type' =>'professeur');
+			$user = array('per_id' => $result[0][0] , 'per_user' =>$result[0][1],'per_password' =>$result[0][2],'per_nom' =>$result[0][3],'per_mail' =>$result[0][4],'pro_id' =>$res[0][0], 'type' =>'professeur');
 		}
 
 		if($i==3)
 		{
-			$user = array('per_id' => $result[0][0] , 'per_user' =>$result[0][1],'per_password' =>$result[0][2],'per_nom' =>$result[0][3],'per_mail' =>$result[0][4],'etu_id' => $res[0][0], 'type' =>'etudiant','ser_id' =>$res[0][0], 'type' =>'service');
+			$user = array('per_id' => $result[0][0] , 'per_user' =>$result[0][1],'per_password' =>$result[0][2],'per_nom' =>$result[0][3],'per_mail' =>$result[0][4],'ser_id' =>$res[0][0], 'type' =>'service');
 		}
 
 
