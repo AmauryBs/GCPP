7531857e03f82e164237dbdd234d08e8ccc76e84
Brynanum <bryan.fauquembergue@etu.univ-smb.fr>
2019-05-17 15:13:18 +0200

    create + update form

--- controller/espaceController.php
+++ controller/espaceController.php
@@ -1,2 +1,20 @@
 <?php
-?>
+	switch($_GET['form']) {
+		case 'demandeCreate' :
+			include '../model.php';
+			$demType=['Etudiant'=>'proposition','Professeur'=>'travaux','Service'=>'commande'];
+			Demande::insert(['dem_type'=>$demType[get_class($_SESSION['utilisateur'])],'dem_titre'=>$_POST['titre'],'dem_message'=>$_POST['message']]);
+			header('Location: .?route=espace');
+		break;
+		case 'demandeUpdate' :
+			include '../model.php';
+			$demType=['Etudiant'=>'proposition','Professeur'=>'travaux','Service'=>'commande'];
+			$dem=new Demande($_POST['id']);
+			$dem->dem_type=$demType[get_class($_SESSION['utilisateur'])];
+			$dem->dem_titre=$_POST['titre'];
+			$dem->dem_message=$_POST['message'];
+			$dem->update();
+			header('Location: .?route=espace');
+		break;
+		default: break;
+	}
